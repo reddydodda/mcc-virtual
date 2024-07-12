b03a67b9089828e0340bf97fe234c26211dbc64b
reddydodda <reddydodda@gmail.com>
2024-07-12 11:26:11 -0500

    update

--- old/coredns_patch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl -n coredns patch service coredns-coredns --type='json' -p='[{"op": "replace", "path": "/spec/ports", "value": [{"name": "udp-53", "port": 53, "protocol": "UDP", "targetPort": 53}]}]'
-kubectl -n coredns patch service coredns-coredns --type='json' -p='[{"op": "replace", "path": "/spec/type", "value":"LoadBalancer"}]'
